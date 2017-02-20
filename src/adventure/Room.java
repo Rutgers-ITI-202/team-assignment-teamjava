e6bd0e7b73e3db3b37fe9285066a528760a399d5
am1785 <allenmo88@gmail.com>
2017-02-20 02:30:15 -0500

    Update Room.java

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -4,35 +4,45 @@ public class Room {
 
 public int [][] RoomArray;
 public boolean hasPlayer;
+public String Name;
+public String Description;
 
 public Room (){
-this.hasPlayer = false;
+this.hasPlayer = true;
 this.RoomArray = new int [5][5];
+this.Name = "The beginning";
+this.Description = "This is the beginning of your adventure! Look around, find Pokeballs, and capture Pokemons to pass roadblocks!";
 }
 
-public String displayRoom(){
+public String getDescription() {
+	return Description;
+}
+
+public String getName() {
+	return Name;
+}
+
+public String displayRoom(Adventurer james){
 		
 	StringBuffer layout = new StringBuffer ("");
 		for(int row = 0; row < RoomArray.length; row++){
 			for(int col = 0; col< RoomArray[0].length; col++){
 				 layout.append("[");
-				if(hasPlayer == true){
+				if(row == james.X && col == james.Y){
 					layout.append(".] ");
 					
-				} else {
-					layout.append("] ");
-					
+				} 
+				
+				if(row!= james.X || col != james.Y){
+					layout.append(" ] ");
 				}
+				
+				
 			}
 			layout.append("\n");
 			}
+		
+		
 		return layout.toString();
 		}
-
-public static void main(String [] args){
-	Room first = new Room();
-	System.out.println(first.displayRoom());
-	
-}
-	
 }
