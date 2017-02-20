0ba6a119f7635e6c5d4530b7d64b09b5f18338fe
am1785 <allenmo88@gmail.com>
2017-02-20 00:24:29 -0500

    Add files via upload
    First attempt at initializing + displaying a 5x5 grid room using an integer double array.

---
+++ src/adventure/Room.java
@@ -0,0 +1,38 @@
+package TextGame;
+
+public class Room {
+
+public int [][] RoomArray;
+public boolean hasPlayer;
+
+public Room (){
+this.hasPlayer = false;
+this.RoomArray = new int [5][5];
+}
+
+public String displayRoom(){
+		
+	StringBuffer layout = new StringBuffer ("");
+		for(int row = 0; row < RoomArray.length; row++){
+			for(int col = 0; col< RoomArray[0].length; col++){
+				 layout.append("[");
+				if(hasPlayer == true){
+					layout.append(".] ");
+					
+				} else {
+					layout.append("] ");
+					
+				}
+			}
+			layout.append("\n");
+			}
+		return layout.toString();
+		}
+
+public static void main(String [] args){
+	Room first = new Room();
+	System.out.println(first.displayRoom());
+	
+}
+	
+}
