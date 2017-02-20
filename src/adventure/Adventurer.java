ca8672f51c626db3ed9ad2de3179e6d9e6035f58
am1785 <allenmo88@gmail.com>
2017-02-20 02:28:03 -0500

    Update Adventurer.java

--- src/adventure/Adventurer.java
+++ src/adventure/Adventurer.java
@@ -1,16 +1,63 @@
 /*
 An adventurer has:
-ï	A location (some room).
-ï	An inventory (the things being carried).  When the player executes the ìtake thingî command, the item should be added to the inventory.
+‚Ä¢	A location (some room).
+‚Ä¢	An inventory (the things being carried).  When the player executes the ‚Äútake thing‚Äù command, the item should be added to the inventory.
 
 An adventurer can:
-ï	Move from room to room.
-ï	Carry a number of objects.
-ï	Pick up, drop, look at, and use various objects.
+‚Ä¢	Move from room to room.
+‚Ä¢	Carry a number of objects.
+‚Ä¢	Pick up, drop, look at, and use various objects.
 */
 
 package adventure;
 
 public class Adventurer {
+public 	int X; // X coordinate position of the adventurer
+public int Y; // Y coordinate position of the adventurer
+
+public Adventurer (){
+	this.X = 0;
+	this.Y = 0;
+}
+
+public void MoveNorth(){
+	X = X-1;
+	if (X < 0) {
+		X = 0;
+		System.out.println("The adventurer cannot move north anymore.");
+		System.out.println();
+	}
+	
+}
+
+public void MoveSouth(){
+	X = X+1;
+	if (X > 4) {
+		X = 4;
+		System.out.println("The adventurer cannot move south anymore.");
+		System.out.println();
+	}
+	
+}
+
+public void MoveWest() {
+	Y = Y-1;
+	if (Y < 0){
+		Y = 0;
+		System.out.println("The adventurer cannot move west anymore.");
+		System.out.println();
+	}
+	
+}
+
+public void MoveEast() {
+	Y = Y+1;
+	if (Y > 4){
+		Y = 4;
+		System.out.println("The adventurer cannot move east anymore.");
+		System.out.println();
+	}
+	
+}
 
 }
