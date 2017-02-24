11de6e4f71c47a3ef85d484a5a155c6af07bc38f
am1785 <allenmo88@gmail.com>
2017-02-24 17:25:51 -0500

    Update Adventurer.java

--- src/adventure/Adventurer.java
+++ src/adventure/Adventurer.java
@@ -12,15 +12,50 @@ An adventurer can:
 package adventure;
 
 public class Adventurer {
+public String name; // Given name to the adventurer
 public 	int X; // X coordinate position of the adventurer
 public int Y; // Y coordinate position of the adventurer
+public ArrayList inventory; // Arraylist that stores the Adventurer's items
+public Pokemons [] pokemons; // Array that sotres Pokemons objects
+public int TeamSize; // How many pokemons adventurer has on team
 
 public Adventurer (){
+	
 	this.X = 0;
 	this.Y = 0;
+	this.name = "James";
+	this.inventory = new ArrayList();
+	this.pokemons = new Pokemons[6]; // the adventurer can have a max team of 6 pokemons
+	this.TeamSize = 0;
 }
 
-public void MoveNorth(){
+public void Pick (Object item){ // Adds item object to inventory ArrayList.
+inventory.add(item);
+}
+
+public void ViewInventory(){ // Views inventory and prints toString representation of inventory ArrayList.
+	System.out.println(name + "'s inventory: " + inventory.toString());
+}
+
+public void CatchPokemon(Pokemons x){
+	
+	if (TeamSize < 7){
+	int counter = (int) (Math.random() * 100);
+	if(counter <= x.CatchRate){
+	System.out.println("Congratulations, the pokemon has been caught!");
+	pokemons[TeamSize] = x;
+	TeamSize++;
+	} else { System.out.println("The pokemon escaped from the pokeball!");
+		
+	}
+	} else { System.out.println("You already have 6 pokemons! No more could be caught at this moment.");
+	}
+	
+	
+}
+
+public void MoveNorth(){ //Updates Adventurer's X coordinate (-1)
+	
 	X = X-1;
 	if (X < 0) {
 		X = 0;
@@ -30,7 +65,7 @@ public void MoveNorth(){
 	
 }
 
-public void MoveSouth(){
+public void MoveSouth(){ //Updates Adventurer's X coordinate (+1)
 	X = X+1;
 	if (X > 4) {
 		X = 4;
@@ -40,7 +75,7 @@ public void MoveSouth(){
 	
 }
 
-public void MoveWest() {
+public void MoveWest() { //Updates Adventurer's Y coordinate (-1)
 	Y = Y-1;
 	if (Y < 0){
 		Y = 0;
@@ -50,13 +85,26 @@ public void MoveWest() {
 	
 }
 
-public void MoveEast() {
+public void MoveEast() { //Updates Adventurer's Y coordinate (+1)
 	Y = Y+1;
 	if (Y > 4){
 		Y = 4;
 		System.out.println("The adventurer cannot move east anymore.");
 		System.out.println();
 	}
+	
+}
+
+public static void main(String [] args){
+	String item = "pokeball";
+	Object water = "potion";
+	Adventurer james = new Adventurer ();
+	james.Pick(item);
+	james.ViewInventory();
+	james.Pick(water);
+	james.ViewInventory();
+	Pokemons pokemon = new Pokemons();
+	james.CatchPokemon(pokemon);
 	
 }
 
