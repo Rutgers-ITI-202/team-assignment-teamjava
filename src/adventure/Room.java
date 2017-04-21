d0955e2d945e2714198ffdde35ee10e2a5902069
am1785 <allenmo88@gmail.com>
2017-04-21 16:46:32 -0400

    Update Room.java

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,74 +1,132 @@
 package adventure;
+import java.util.*;
+
 public class Room {
 
-public int [][] RoomArray;
-public boolean hasPlayer;
-public String Name;
-public String Description;
-public String Stage;
-Pokemons Pika;
+private int [][] RoomArray;
+private boolean hasPlayer;
+private String Name;
+private String Description;
+private ArrayList <obstacle> Ob;
 
 
 public Room (String name){
 this.hasPlayer = true;
 this.RoomArray = new int [5][5];
-this.Name = "The beginning";
-this.Description = "This is the beginning of your adventure! Look around, find Pokeballs, and capture Pokemons to pass roadblocks!";
-this.Stage = name;
+this.Name = name;
+this.Ob = new ArrayList<obstacle> ();
+this.setDescription("This is the beginning of your adventure! Look around, find Pokeballs, and capture Pokemons to pass roadblocks!");
 }
 
-public void setStage(String name){
-Stage = name;
+public void setRoomArray(int [][] x){
+	RoomArray = x;
 }
 
-/**public void Initialize (){
+public int [][] getArray(){
+	return RoomArray;
+}
 
-	if(Stage.equalsIgnoreCase("Kanto")){
-		RoomArray = new int [5][5];
-	}
-	
-	if(Stage.equalsIgnoreCase("Hoenn")){
-		RoomArray = new int [6][6];
-	}
-	
-}*/ 
-
+public void setDescription(String s){
+	Description = s;
+}
 
 public String getDescription() {
 	return Description;
 }
 
-public String getName() {
+public void setName(String s){
+	Name = s;
+}
+
+public String getName() { // gets room name
 	return Name;
 }
 
-public String displayRoom(Adventurer james, Pokemons Pika){
-		
+public boolean playerPresent(){
+	return hasPlayer;
+}
+
+public void addObstacle(obstacle ob){
+	Ob.add(ob);
+}
+
+public ArrayList<obstacle> getObstacles(){
+	return Ob;
+}
+
+public String displayRoom(Adventurer james, Pokemons Pika, Items item){
+	boolean hasX = false;
 	StringBuffer layout = new StringBuffer ("");
 		for(int row = 0; row < RoomArray.length; row++){
 			for(int col = 0; col< RoomArray[0].length; col++){
 				 layout.append("[");
-				if(row == james.X && col == james.Y){
+				if(row == james.getX() && col == james.getY()){
 					layout.append(".] ");
-					
-				} else if(row == Pika.X && col == Pika.Y){
+					continue;
+				 } 
+				
+				else if(row == Pika.getX() && col == Pika.getY()){
 					layout.append("P] ");
+					continue;
 				}
 				
-				else if((row!= james.X || col != james.Y)){
+				else if(row == item.getX() && col == item.getY()){
+					layout.append("I] ");
+					continue;
+				}
+				
+				for(int i = 0; i < Ob.size(); i++){
+					if(Ob.get(i).getX() == row && Ob.get(i).getY() == col){
+						layout.append("X] ");
+						hasX = true;
+						continue;
+					}
+				}
+			
+				if(hasX == true){
+					hasX = false;
+					continue;
+				}
+			
+				else if ((row!= james.getX() || col != james.getY())){
 					layout.append(" ] ");
+					
 				} 
 			}
 			layout.append("\n");
 			}
 		
+		if(james.getX() == Pika.getX() && james.getY() == Pika.getY()){
+			System.out.println("There is a pokemon nearby, take a look around!" + "\n"); 
+		 }
+		
+		if(james.getX() == item.getX() && james.getY() == item.getY()){
+			System.out.println("There is an item nearby, take a look around!");
+		}
+	
+		
 		return layout.toString();
 		}
+
+
 
 public static void main(String [] args){
 	Room first = new Room("Hoenn");
 	Adventurer james = new Adventurer();
 	Pokemons Pika = new Pokemons();
-	System.out.println(first.displayRoom(james, Pika));
+	sitrusBerry berry = new sitrusBerry();
+	obstacle ob = new obstacle(3,3);
+	first.addObstacle(ob);
+	System.out.println(first.displayRoom(james, Pika, berry));
+	james.MoveEast(first);
+	james.MoveSouth(first);
+	System.out.println(first.displayRoom(james, Pika, berry));
+	james.MoveSouth(first);
+	james.MoveSouth(first);
+	james.MoveEast(first);
+	james.MoveEast(first);
+	System.out.println(first.displayRoom(james, Pika, berry));
+	
+	
 }
 }
