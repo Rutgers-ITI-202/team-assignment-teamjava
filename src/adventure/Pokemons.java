da62d9c2c3e16f4913332e24044520809b085708
am1785 <allenmo88@gmail.com>
2017-04-21 16:44:23 -0400

    Update Pokemons.java

--- src/adventure/Pokemons.java
+++ src/adventure/Pokemons.java
@@ -2,32 +2,92 @@ package adventure;
 import java.util.*;
 
 public class Pokemons {
-public int X;
-public int Y;
-public double CatchRate; //double value that determines how likely a pokemon could be caught using a pokemon
-public String Description; //String that describes the pokemon 
-public String Ability; //String representation of a pokemon's abilities
-public String name; //Name of the pokemon Stored as a String
-public boolean CanSurf; //boolean that determines if a pokemon can use ability: Surf
-public boolean CanCut; //boolean that determines if a pokemon can use ability: Cut 
-public boolean CanRockSmash; //boolean that determines if a pokemon can use ability: Rock Smash
-public boolean CanDive; //boolean that determines if a pokemon can use ability: Dive
+private int X;
+private int Y;
+
+public String Description; //String that describes the pokemon
+public String dialogue;
+
+private String Ability; //String representation of a pokemon's abilities
+private String name; //Name of the pokemon Stored as a String
+private boolean CanSurf; //boolean that determines if a pokemon can use ability: Surf
+private boolean CanCut; //boolean that determines if a pokemon can use ability: Cut 
+private boolean CanRockSmash; //boolean that determines if a pokemon can use ability: Rock Smash
+private boolean CanDive; //boolean that determines if a pokemon can use ability: Dive
+private boolean CanFly;
 
 public Pokemons () {
-this.X = 4;	
-this.Y = 4;
-this.CatchRate = 50; // 50% catch rate
+this.X = 0;	
+this.Y = 1;
+this.dialogue = "Hi, I am" + name + ", nice to meet you!";
 this.Ability = "CUT";
 this.Description = "[name of pokemon], this pokemon knows how to use ability " + Ability + "."; 
 this.name = "First Pokemon";
-this.CanCut = true;
+this.CanCut = false;
 this.CanDive = false;
 this.CanRockSmash = false;
 this.CanSurf = false;
+this.CanFly = false;
+}
+
+public void setName(String N){
+	name = N;
+}
+
+public String getName(){
+	return name;
+}
+
+public boolean canSurf(){
+	return CanSurf;
+}
+
+public boolean canCut(){
+	return CanCut;
+}
+
+public boolean canRockSmash(){
+	return CanRockSmash;
+}
+
+public boolean canFly(){
+	return CanFly;
+}
+
+public void setFly(){
+	CanFly = true;
+}
+
+public void setSurf(){
+	CanSurf = true;
+}
+
+public void setRockSmash(){
+	CanRockSmash = true;
+}
+
+public void setCut(){
+	CanCut = true;
+}
+
+public void setX(int x){
+	X = x;
+}
+
+public void setY(int y){
+	Y = y;
+}
+
+public int getX(){
+	return X;
+}
+
+public int getY(){
+	return Y;
 }
 
 public static void main(String [] args){
-Pokemons pikachu = new Pokemons();
+
 }
 
 }
