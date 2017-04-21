bcd400288308ccd66a482e960c144ccdbe837621
am1785 <allenmo88@gmail.com>
2017-04-21 16:43:08 -0400

    Add files via upload

---
+++ src/adventure/pidgey.java
@@ -0,0 +1,13 @@
+package TextGame;
+
+public class pidgey extends Pokemons {
+
+public pidgey(int x, int y){
+this.Description = "Bird type Pokemon, seems like it knows how to use fly";
+this.setFly();
+this.setX(x);
+this.setY(y);
+this.setName("Pidgey");
+}
+	
+}
