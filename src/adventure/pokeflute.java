e9516884835f1394b88b8b53cb78b63ac6b6a50e
am1785 <allenmo88@gmail.com>
2017-04-27 03:17:47 -0400

    Add files via upload

---
+++ src/adventure/pokeflute.java
@@ -0,0 +1,39 @@
+package TextGame;
+
+public class pokeflute extends Items{
+
+	private String functionToString;
+	
+	public pokeflute(int x, int y){
+	
+	this.setX(x);
+	this.setY(y);
+	this.setName("Poke Flute");
+	this.setDescription("Flute that produces beautiful melodies when played.");
+	this.functionToString = "Snorlax has been waken up gently and slowly opened its eyes.";
+	}
+
+
+
+public String functionToString(){
+	return functionToString;
+}
+
+public void function(Adventurer Ditto, Room room){
+	boolean SnorlaxFound = false;
+	for(int i = 0; i < room.getPokemons().size(); i ++){
+		if(room.getPokemons().get(i) instanceof snorlax && Ditto.getX() == room.getPokemons().get(i).getX() && Ditto.getY() == room.getPokemons().get(i).getY()){
+			((snorlax)(room.getPokemons().get(i))).Gentle = true;
+			SnorlaxFound = true;
+			System.out.println(Ditto.getName() + " used poke flute, Snorlax is gently waken up and slowly opening its eyes!");
+			System.out.println();
+		}
+	}
+	
+	if(SnorlaxFound == false){
+		System.out.println("You cannot use poke flute here!");
+		System.out.println();
+	}
+}
+
+}
