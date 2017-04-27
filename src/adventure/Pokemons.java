d72171f5fa8736926e6521a5473a0eba1fcd05ff
am1785 <allenmo88@gmail.com>
2017-04-27 03:18:36 -0400

    Update Pokemons.java

--- src/adventure/Pokemons.java
+++ src/adventure/Pokemons.java
@@ -6,15 +6,15 @@ private int X;
 private int Y;
 
 public String Description; //String that describes the pokemon
-public String dialogue;
+public String dialogue; // String and interaction of Pokemons when talked to by the Adventurer
 
 private String Ability; //String representation of a pokemon's abilities
 private String name; //Name of the pokemon Stored as a String
-private boolean CanSurf; //boolean that determines if a pokemon can use ability: Surf
-private boolean CanCut; //boolean that determines if a pokemon can use ability: Cut 
-private boolean CanRockSmash; //boolean that determines if a pokemon can use ability: Rock Smash
-private boolean CanDive; //boolean that determines if a pokemon can use ability: Dive
-private boolean CanFly;
+protected boolean CanSurf; //boolean that determines if a pokemon can use ability: Surf
+protected boolean CanCut; //boolean that determines if a pokemon can use ability: Cut 
+protected boolean CanRockSmash; //boolean that determines if a pokemon can use ability: Rock Smash
+protected boolean CanDive; //boolean that determines if a pokemon can use ability: Dive
+protected boolean CanFly; //boolean that determines if a pokemon can use the ability: Fly
 
 public Pokemons () {
 this.X = 0;	
@@ -30,12 +30,20 @@ this.CanSurf = false;
 this.CanFly = false;
 }
 
+public String getDescription(){
+	return Description;
+}
+
 public void setName(String N){
 	name = N;
 }
 
 public String getName(){
 	return name;
+}
+
+public void showDialogue(){
+	System.out.println(dialogue);
 }
 
 public boolean canSurf(){
