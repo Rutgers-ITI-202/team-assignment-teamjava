75e388964278a86ce60effb57cc276941948fac5
am1785 <allenmo88@gmail.com>
2017-04-27 03:21:49 -0400

    Update sky.java

--- src/adventure/sky.java
+++ src/adventure/sky.java
@@ -4,8 +4,8 @@ public class sky extends obstacle {
 
 	public sky(int x, int y) {
 		super(x, y);
-		this.setDescription("Aerial terrain ahead, only if I can use 'fly' to get across...");
-		
+		this.setDescription("Aerial terrain ahead, if only I can use 'fly' to get across..." + "\n");
+		this.passAbility = "Fly";
 	}
 
 
